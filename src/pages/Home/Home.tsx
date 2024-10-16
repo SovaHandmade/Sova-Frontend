fbaead5c76250afaa171f9285c887cf6b0f4d1af
Andrii <kalenskuja@gmail.com>
2024-10-16 16:45:47 +0200

    Implement home page layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -87,11 +87,103 @@ export const Home = () => {
             alt="Product photo"
           />
           <img
+            className="home__colors-arrow home__colors-arrow--left"
+            src="arrow_drawing.svg"
+            alt="Arrow"
+          />
+          <img
             className="home__image home__colors-image--small"
             src="product-photo/1.jpg"
             alt="Product photo"
           />
+          <img
+            className="home__colors-arrow home__colors-arrow--right"
+            src="arrow_drawing.svg"
+            alt="Arrow"
+          />
         </div>
+      </section>
+
+      <section className="home__section home__faq">
+        <h2 className="home__faq-title">Як все працює?</h2>
+
+        <div className="home__faq-container">
+          <div className="home__faq-question">
+            <div className="home__faq-question-top">
+              <div className="home__faq-question-number">1</div>
+              <div className="home__faq-question-info">
+                <h4 className="home__faq-question-title">
+                  Вибери комозицію та оформи замовлення
+                </h4>
+                <p className="small-text home__faq-question-subtitle">
+                  Лише кілька простих кроків, і все зроблено
+                </p>
+              </div>
+            </div>
+            <img
+              className="home__faq-question-image"
+              src="images/order.png"
+              alt="Order image"
+            />
+          </div>
+
+          <div className="home__faq-question">
+            <div className="home__faq-question-top">
+              <div className="home__faq-question-number">2</div>
+              <div className="home__faq-question-info">
+                <h4 className="home__faq-question-title">
+                  Ми зв’яжимось з тобою
+                </h4>
+                <p className="small-text home__faq-question-subtitle">
+                  Домовимось про деталі, доставку та оплату
+                </p>
+              </div>
+            </div>
+            <img
+              className="home__faq-question-image"
+              src="images/phone.png"
+              alt="Order image"
+            />
+          </div>
+
+          <div className="home__faq-question">
+            <div className="home__faq-question-top">
+              <div className="home__faq-question-number">3</div>
+              <div className="home__faq-question-info">
+                <h4 className="home__faq-question-title">
+                  Створемо вашу композицію всього за 1 день
+                </h4>
+                <p className="small-text home__faq-question-subtitle">
+                  Швидко і з любов'ю.
+                </p>
+              </div>
+            </div>
+            <img
+              className="home__faq-question-image"
+              src="images/work.png"
+              alt="Order image"
+            />
+          </div>
+
+          <div className="home__faq-question">
+            <div className="home__faq-question-top">
+              <div className="home__faq-question-number">4</div>
+              <div className="home__faq-question-info">
+                <h4 className="home__faq-question-title">Доставка поштою.</h4>
+                <p className="small-text home__faq-question-subtitle">
+                  Насолоджуйтесь своїм оновленим простором!
+                </p>
+              </div>
+            </div>
+            <img
+              className="home__faq-question-image"
+              src="images/deliver.png"
+              alt="Order image"
+            />
+          </div>
+        </div>
+
+        <button className="home__faq-button">Перейти до товарів</button>
       </section>
     </main>
   );
