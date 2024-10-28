a4a736e01a284bd86e83f4dd869de222aa4ccbdf
Andrii <kalenskuja@gmail.com>
2024-10-28 10:08:40 +0100

    Adapt shop page for small desktop

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,13 +1,33 @@
+import { useState } from "react";
+import classNames from "classnames";
 import { FilterBox } from "../../components/FilterBox";
 import { ProductCard } from "../../components/ProductCard";
 import "./Shop.scss";
 
 export const Shop = () => {
+  const [showFilters, setShowFilters] = useState(false);
+
+  const handleFiltersButton = () => {
+    setShowFilters(!showFilters);
+  };
+
   return (
     <section className="shop">
-      <h2>Our products</h2>
+      <div className="shop__top">
+        <h2>Our products</h2>
+        <button className="shop__filters-button" onClick={handleFiltersButton}>
+          Filters
+        </button>
+      </div>
+
       <div className="shop__container">
-        <FilterBox />
+        <div
+          className={classNames("shop__filter-box", {
+            "shop__filter-box--show": showFilters,
+          })}
+        >
+          <FilterBox />
+        </div>
         <div className="shop__products-container">
           <ProductCard />
           <ProductCard />
