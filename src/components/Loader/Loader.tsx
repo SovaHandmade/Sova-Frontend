8d06983f01d16d7cdc26bb949614256e2db3b1d0
Andrii <kalenskuja@gmail.com>
2024-11-13 09:24:07 +0100

    Fix missing loader image and remove centered class

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const Loader: React.FC<Props> = ({ isSmall = false }) => {
   return (
-    <div className="loader centered">
+    <div className="loader">
       <img
         src="/loader.png"
         alt="Loader"
