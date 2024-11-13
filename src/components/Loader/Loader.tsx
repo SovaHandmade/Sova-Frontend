36d21b7b1e03a85ff98cfd2aa27e20f6a4df2f81
Andrii <kalenskuja@gmail.com>
2024-11-13 09:12:40 +0100

    Create Loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,21 @@
+import classNames from "classnames";
+import "./Loader.scss";
+import React from "react";
+
+type Props = {
+  isSmall?: boolean;
+};
+
+export const Loader: React.FC<Props> = ({ isSmall = false }) => {
+  return (
+    <div className="loader">
+      <img
+        src="/loader.png"
+        alt="Loader"
+        className={classNames("loader__spinner", {
+          "loader__spinner--small": isSmall,
+        })}
+      />
+    </div>
+  );
+};
