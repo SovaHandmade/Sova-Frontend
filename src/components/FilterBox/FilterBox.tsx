6d0fa98b89988c2f86c8a25a9d6136cd73ff633d
Andrii <kalenskuja@gmail.com>
2024-11-13 18:27:39 +0100

    Add loader to FilterBox

--- src/components/FilterBox/FilterBox.tsx
+++ src/components/FilterBox/FilterBox.tsx
@@ -4,6 +4,7 @@ import { getTags } from "../../api/api";
 import { Tags } from "../../types/Tags";
 import { useSearchParams } from "react-router-dom";
 import "./FilterBox.scss";
+import { Loader } from "../Loader";
 
 type Props = {
   showButtons?: boolean;
@@ -85,57 +86,63 @@ export const FilterBox: React.FC<Props> = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (!tags) {
-    return <></>;
-  }
-
   return (
     <div className="filter-box">
-      <div className="filter-box__container">
-        <p className="small-text">Тема</p>
-        <div className="filter-box__filters">
-          {tags.topics.map((topic, index) => (
-            <Filter
-              name={topic.name}
-              key={index}
-              selected={
-                selectedTopic === ""
-                  ? index === 0
-                  : topic.name === selectedTopic
-              }
-              selectCallback={setSelectedTopic}
-            />
-          ))}
-        </div>
-      </div>
-      <div className="filter-box__container">
-        <p className="small-text">Форма</p>
-        <div className="filter-box__filters">
-          {tags.forms.map((form, index) => (
-            <Filter
-              name={form.name}
-              key={index}
-              selected={
-                selectedForm === "" ? index === 0 : form.name === selectedForm
-              }
-              selectCallback={setSelectedForm}
-            />
-          ))}
-        </div>
-      </div>
-
-      {showButtons && (
-        <div className="filter-box__buttons">
-          <button
-            className="filter-box__clear-button button--text"
-            onClick={handleClear}
-          >
-            Очистити
-          </button>
-          <button className="filter-box__apply-button" onClick={handleApply}>
-            Застосувати
-          </button>
-        </div>
+      {!tags ? (
+        <Loader isSmall={true} />
+      ) : (
+        <>
+          <div className="filter-box__container">
+            <p className="small-text">Тема</p>
+            <div className="filter-box__filters">
+              {tags.topics.map((topic, index) => (
+                <Filter
+                  name={topic.name}
+                  key={index}
+                  selected={
+                    selectedTopic === ""
+                      ? index === 0
+                      : topic.name === selectedTopic
+                  }
+                  selectCallback={setSelectedTopic}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="filter-box__container">
+            <p className="small-text">Форма</p>
+            <div className="filter-box__filters">
+              {tags.forms.map((form, index) => (
+                <Filter
+                  name={form.name}
+                  key={index}
+                  selected={
+                    selectedForm === ""
+                      ? index === 0
+                      : form.name === selectedForm
+                  }
+                  selectCallback={setSelectedForm}
+                />
+              ))}
+            </div>
+          </div>
+          {showButtons && (
+            <div className="filter-box__buttons">
+              <button
+                className="filter-box__clear-button button--text"
+                onClick={handleClear}
+              >
+                Очистити
+              </button>
+              <button
+                className="filter-box__apply-button"
+                onClick={handleApply}
+              >
+                Застосувати
+              </button>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
